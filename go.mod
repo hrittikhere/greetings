73ca196a41064903491b7caab1b195c1a46f1328
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-09-20 19:24:25 +0000

    Add changes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hrittikhere/greetings
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hey Welcome")
+
+	return message
+}
